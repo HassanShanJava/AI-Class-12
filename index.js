f314eaff8e74fa0db3127b399f666f71177a0333
HassanShanJava <hassanshan675@gmail.com>
2022-12-01 19:01:05 +0500

    backgrounf color

--- index.js
+++ index.js
@@ -11,49 +11,35 @@ function convertion(val) {
   return (val - 273).toFixed(2);
 }
 
-btn.addEventListener("click", function () {
-  fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      inputval.value +
-      "&appid=" +
-      apik
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      var nameval = data["name"];
-      var descrip = data["weather"]["0"]["description"];
-      var tempature = data["main"]["temp"];
-      var wndspd = data["wind"]["speed"];
-
-      city.innerHTML = `Weather of <span>${nameval}<span>`;
-      temp.innerHTML = `Temperature: <span>${convertion(tempature)} C</span>`;
-      description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
-      wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
-    })
-    .catch((err) => alert("Wrong City"));
-});
-
-btn.addEventListener("keypress", function (e) {
-    if(e.key === 'Enter'){
-        e.preventDefault()
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-              inputval.value +
-              "&appid=" +
-              apik
-          )
-            .then((res) => res.json())
-            .then((data) => {
-              var nameval = data["name"];
-              var descrip = data["weather"]["0"]["description"];
-              var tempature = data["main"]["temp"];
-              var wndspd = data["wind"]["speed"];
-        
-              city.innerHTML = `Weather of <span>${nameval}<span>`;
-              temp.innerHTML = `Temperature: <span>${convertion(tempature)} C</span>`;
-              description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
-              wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
-            })
-            .catch((err) => alert("Wrong City"));
+function click() {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        inputval.value +
+        "&appid=" +
+        apik
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        var nameval = data["name"];
+        var descrip = data["weather"]["0"]["description"];
+        var tempature = data["main"]["temp"];
+        var wndspd = data["wind"]["speed"];
+  
+        city.innerHTML = `Weather of <span>${nameval}<span>`;
+        temp.innerHTML = `Temperature: <span>${convertion(tempature)} C</span>`;
+        description.innerHTML = `Sky Conditions: <span>${descrip}<span>`;
+        wind.innerHTML = `Wind Speed: <span>${wndspd} km/h<span>`;
+        document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + inputval.value + "')";
+      })
+      .catch((err) => alert("Wrong City"));
+  }
+btn.addEventListener("keypress", (e)=> {
+    if(e.keyCode === 13){
+        // e.preventDefault()
+        click()
     }
-  });
+},false);
+
+btn.addEventListener("click", click);
+
